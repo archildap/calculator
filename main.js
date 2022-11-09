7a82d8bf128538a293099bf3c5ac3a983d9aa648
Archil Dapkviashvili <archil.dapkviashvili@gmail.com>
2022-11-09 14:31:45 +0400

    Add operate func and buttons from 0-9 +-*/=

--- main.js
+++ main.js
@@ -13,3 +13,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(oper, num1, num2) {
+  if(oper === '+') {
+    return add(num1, num2);
+  } else if(oper === '-') {
+    return subtract(num1, num2);
+  } else if(oper === '*') {
+    return multiply(num1, num2);
+  } else if(oper === '/') {
+    return divide(num1, num2);
+  }
+}
