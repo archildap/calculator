b257697fb292d7a650c09e9f359791e2630755d9
Archil Dapkviashvili <archil.dapkviashvili@gmail.com>
2022-11-11 12:11:03 +0400

    Add function that populates display and calculates it on click 0f =

--- main.js
+++ main.js
@@ -25,3 +25,40 @@ function operate(oper, num1, num2) {
     return divide(num1, num2);
   }
 }
+
+const buttons = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.oper');
+const equalOper = document.querySelector('.equals');
+const dispNum = document.querySelector('.num');
+const btnClear = document.querySelector('.clear');
+
+buttons.forEach(num => {
+  num.addEventListener('click', e => {
+    dispNum.textContent += num.textContent;
+  });
+});
+
+let num1;
+let num2;
+let op;
+
+operators.forEach(op => {
+  op.addEventListener('click', e => {
+    num1 = dispNum.textContent;
+    dispNum.textContent += op.textContent;
+  });
+});
+
+equalOper.addEventListener('click', e => {
+  arrayNum = dispNum.textContent.split('');
+  num2 = arrayNum.slice(num1.length + 1).join('');
+  op = arrayNum.slice(num1.length, num1.length + 1).join('');
+  dispNum.textContent = operate(op, +num1, +num2);
+});
+
+btnClear.addEventListener('click', e => {
+  dispNum.textContent = '';
+});
+
+
+
